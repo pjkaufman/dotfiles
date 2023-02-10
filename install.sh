723f363fb77400711c0a9a6e4c0474823a856a9c
Peter Kaufman <PeterJamesKaufman@gmail.com>
2023-02-10 16:47:11 -0500

    updated the setup for the install script some more

--- install.sh
+++ install.sh
@@ -36,56 +36,158 @@ install_flatpak_package() {
   fi
 }
 
-# add_ppa_and_install_package adds the specified PPA if 
+# add_ppa_and_install_package adds the specified PPA if it does not exist already
+# $1 is the name of the package with a slash and the type of stability of the PPA (i.e. syncthing/stable)
+# $2 is the name of the package to install once the PPA has been added
 add_ppa_and_install_package() {
+
+  grep_output=`apt-cache policy| grep $1`
+  if [ -z grep_output ]
+  then
+    echo "adding $1 PPA and installing $2"
+
+    sudo add-apt-repository ppa:$1
+    sudo apt update && sudo apt install $2
+  else
+    echo "$1 PPA already added"
+  fi
+}
+
+pip_install_package() {
+
+  if ! command -v $1 &> /dev/null
+  then
+    echo "installing $1"
+    pip3 install --user $1  
+  else
+    echo "$1 is already installed"
+  fi
 
 }
 
+# handle_flatpak_installations determines whether or not to install the flatpaks
+# and installs any that are missing
+# $1 is whether or not the computer is a work computer
+handle_flatpak_installations() {
+
+  if $1
+  then
+    if ! command -v flatpak &> /dev/null
+    then
+      echo "Flatpak not installed. Please install it."
+    else
+      install_flatpak_package "Brave Browser" "com.brave.Browser"
+      install_flatpak_package "Minecraft" "com.mojang.Minecraft"
+      install_flatpak_package "GnuCash" "com.gnucash.GnuCash"
+      install_flatpak_package "Sigil" "com.sigil_ebook.Sigil"
+      install_flatpak_package "Calibre" "com.calibre_ebook.calibre"
+      install_flatpak_package "Obsidian" "md.obsidian.Obsidian"
+      install_flatpak_package "Only Office" "org.onlyoffice.desktopeditors"
+    fi
+  else
+    echo "Skipping flatpak installation"
+  fi
+}
+
 setup_header_text() {
+  echo ""
+  echo "==============================="
   echo "$1"
+  echo "==============================="
   echo ""
 }
 
 # actual setup
+
 echo "starting environment setup"
-echo ""
 
-# TODO: determine whether or not this env is work or personal
+# determine whether or not this env is work or personal
 
-setup_header_text "installing apt packages:"
+setup_header_text "get computer type:"
+
+is_work_computer=false
+if [ -z "${COMPUTER_TYPE}" ]
+then
+  read -p 'Is this a personal computer? [y]es or [n]o: ' response_char
+
+  if [ response_char = "y" ]
+  then
+    is_work_computer=false
+    echo 'export COMPUTER_TYPE=personal' >> ~/.bashrc 
+  else
+    is_work_computer=true
+    echo 'export COMPUTER_TYPE=work' >> ~/.bashrc 
+  fi
+
+  echo "Please make sure to run source your profile after the install."
+else
+  if [ ${COMPUTER_TYPE} = "personal" ]
+  then
+    is_work_computer=false
+  else
+    is_work_computer=true
+  fi
+
+  echo "The computer is a ${COMPUTER_TYPE} one."
+fi
 
 # apt packages
+
+setup_header_text "apt packages:"
 
 install_apt_package "git"
 install_apt_package "tmux"
 install_apt_package "grep"
-install_apt_package "btop"
+install_apt_package "curl"
+install_apt_package "btop" # task manager equivalent
 install_apt_package "python3"
 install_apt_package "pip3" "python3-pip"
 
-# TODO: PPA additions
+if $is_work_computer
+then
+  install_apt_package "imgp" # image compression
+  install_apt_package "pandoc" # document conversion
+  install_apt_package "flameshot" # screenshots
+  install_apt_package "kitty" # terminal
+fi
+
+# PPA additions
+
+setup_header_text "PPA additions"
+
+if $is_work_computer
+then 
+  add_ppa_and_install_package "syncthing/stable" "syncthing"
+fi
+
+add_ppa_and_install_package "neovim-ppa/unstable" "neovim"
 
 # flatpak packages
 
-setup_header_text "installing flatpak packages:"
+setup_header_text "Flatpak packages:"
 
-if ! command -v flatpak &> /dev/null
-then
-  echo "Flatpak not installed. Please install it."
-else
-  install_flatpak_package "Brave Browser" "com.brave.Browser"
-  install_flatpak_package "Minecraft" "com.mojang.Minecraft"
-  install_flatpak_package "GnuCash" "com.gnucash.GnuCash"
-  install_flatpak_package "Sigil" "com.sigil_ebook.Sigil"
-  install_flatpak_package "Calibre" "com.calibre_ebook.calibre"
-  install_flatpak_package "Obsidian" "md.obsidian.Obsidian"
-  install_flatpak_package "Only Office" "org.onlyoffice.desktopeditors"
-fi
-
+handle_flatpak_installations $is_work_computer
 
 # TODO: special package managers like nvm and gvm
 
-# TODO: i3 install
+# i3 setup
+
+setup_header_text "i3 setup:"
+
+if $is_work_computer
+then 
+  install_apt_package "i3"
+  install_apt_package "scrot" # screenshots
+  install_apt_package "light" # brightness changes
+  install_apt_package "feh" # background image
+  install_apt_package "i3lock" # lockscreen setup
+  install_apt_package "i3status" # status info
+  install_apt_package "pactl" # sound changes
+  install_apt_package "dmenu" # app selector
+  pip_install_package "bumblebee-status" # status bar
+else
+  echo "skipping i3 setup"
+fi
 
 # TODO: move scripts to bin
 

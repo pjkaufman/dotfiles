d504984508ca6d05b66abbd726fbba17f726b250
Peter Kaufman <PeterJamesKaufman@gmail.com>
2023-03-08 08:10:16 -0500

    added completing for unimported values

--- nvim/lua/user/lsp/settings/gopls.lua
+++ nvim/lua/user/lsp/settings/gopls.lua
@@ -1,23 +1,24 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/gopls.lua
 return {
-	cmd = { "gopls" },
-	settings = {
-		gopls = {
-			buildFlags = { "-tags=unit integration" },
-			usePlaceholders = false,
-			analyses = {
-				nilness = true,
-				shadow = true,
-				unusedparams = true,
-				unusewrites = true,
-			},
-			staticcheck = true,
-			codelenses = {
-				generate = false,
-				gc_details = true,
-				test = true,
-				tidy = true,
-			},
-		},
-	},
+    cmd = { "gopls" },
+    settings = {
+        gopls = {
+            buildFlags = { "-tags=unit integration" },
+            usePlaceholders = false,
+            completeUnimported = true,
+            analyses = {
+                nilness = true,
+                shadow = true,
+                unusedparams = true,
+                unusewrites = true,
+            },
+            staticcheck = true,
+            codelenses = {
+                generate = false,
+                gc_details = true,
+                test = true,
+                tidy = true,
+            },
+        },
+    },
 }

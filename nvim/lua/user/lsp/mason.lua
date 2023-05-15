1a0c204ee0c63ae2eef3f848f6493d5d332f87d4
Peter Kaufman <PeterJamesKaufman@gmail.com>
2023-05-15 10:03:48 -0400

    updated a couple of plugins including lsp which required me to move to lua_ls instead of sumneko_lua

--- nvim/lua/user/lsp/mason.lua
+++ nvim/lua/user/lsp/mason.lua
@@ -1,64 +1,64 @@
 local servers = {
-	"sumneko_lua",
-	"cssls",
-	"html",
-	"tsserver",
-	"pyright",
-	"gopls",
-	"bashls",
-	"jsonls",
-	"yamlls",
+    "lua_ls",
+    "cssls",
+    "html",
+    "tsserver",
+    "pyright",
+    "gopls",
+    "bashls",
+    "jsonls",
+    "yamlls",
 }
 
 local settings = {
-	ui = {
-		border = "none",
-		icons = {
-			package_installed = "◍",
-			package_pending = "◍",
-			package_uninstalled = "◍",
-		},
-	},
-	log_level = vim.log.levels.INFO,
-	max_concurrent_installers = 4,
+    ui = {
+        border = "none",
+        icons = {
+            package_installed = "◍",
+            package_pending = "◍",
+            package_uninstalled = "◍",
+        },
+    },
+    log_level = vim.log.levels.INFO,
+    max_concurrent_installers = 4,
 }
 
 require("mason").setup(settings)
 require("mason-lspconfig").setup({
-	ensure_installed = servers,
-	automatic_installation = true,
+    ensure_installed = servers,
+    automatic_installation = true,
 })
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
-	vim.notify("failed to load lspconfig plugin", vim.log.levels.ERROR)
-	return
+  vim.notify("failed to load lspconfig plugin", vim.log.levels.ERROR)
+  return
 end
 
 local opts = {}
 
 for _, server in pairs(servers) do
-	opts = {
-		on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilities,
-		root_dir = function(fname)
-			local util = require("lspconfig").util
-			return util.root_pattern(".git")(fname)
-				or util.root_pattern("tsconfig.base.json")(fname)
-				or util.root_pattern("package.json")(fname)
-				or util.root_pattern(".eslintrc.js")(fname)
-				or util.root_pattern(".eslintrc.json")(fname)
-				or util.root_pattern("tsconfig.json")(fname)
-				or util.root_pattern("go.mod")(fname)
-		end,
-	}
+  opts = {
+      on_attach = require("user.lsp.handlers").on_attach,
+      capabilities = require("user.lsp.handlers").capabilities,
+      root_dir = function(fname)
+        local util = require("lspconfig").util
+        return util.root_pattern(".git")(fname)
+            or util.root_pattern("tsconfig.base.json")(fname)
+            or util.root_pattern("package.json")(fname)
+            or util.root_pattern(".eslintrc.js")(fname)
+            or util.root_pattern(".eslintrc.json")(fname)
+            or util.root_pattern("tsconfig.json")(fname)
+            or util.root_pattern("go.mod")(fname)
+      end,
+  }
 
-	server = vim.split(server, "@")[1]
+  server = vim.split(server, "@")[1]
 
-	local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
-	if require_ok then
-		opts = vim.tbl_deep_extend("force", conf_opts, opts)
-	end
+  local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
+  if require_ok then
+    opts = vim.tbl_deep_extend("force", conf_opts, opts)
+  end
 
-	lspconfig[server].setup(opts)
+  lspconfig[server].setup(opts)
 end

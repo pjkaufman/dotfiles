2a30975cde0cf3c271deadbc206ff3ae98639ef2
Peter Kaufman <PeterJamesKaufman@gmail.com>
2023-10-15 12:00:57 -0400

    fixed up some issues with tmux I found on my personal computer

--- install/setup_tmux.sh
+++ install/setup_tmux.sh
@@ -12,9 +12,9 @@ ensure_file_symlink_is_in_place "$HOME/dotfiles/tmux/tmux.conf" "$tmux_dir/tmux.
 install_apt_package "tmux"
 
 # install package manager for tmux
-tmux_plugin_dir="$HOME/.tmux/plugins/tpm"
+tmux_plugin_dir="$HOME/.local/share/tmux/plugins/tpm"
 if [ ! -d "$tmux_plugin_dir" ]; then 
-  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+  git clone https://github.com/tmux-plugins/tpm $tmux_plugin_dir
   # install plugins right after tpm is installed
-  $HOME/.tmux/plugins/tpm/scripts/install_plugins.sh
+  $tmux_plugin_dir/scripts/install_plugins.sh
 fi

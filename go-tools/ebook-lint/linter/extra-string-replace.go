5d4863ff095fe063644e451ed1dd2f5931063068
Peter Kaufman <PeterJamesKaufman@gmail.com>
2024-01-25 09:01:55 -0500

    swap from replace all to strings.Replacer since that should be more perfromant

--- go-tools/ebook-lint/linter/extra-string-replace.go
+++ go-tools/ebook-lint/linter/extra-string-replace.go
@@ -4,6 +4,9 @@ import "strings"
 
 func ExtraStringReplace(text string, extraFindAndReplaces map[string]string, numHits map[string]int) string {
 	var newText = text
+
+	var stringsToReplace []string = make([]string, 2*len(extraFindAndReplaces))
+	var i = 0;
 	for search, replace := range extraFindAndReplaces {
 		if hits, ok := numHits[search]; ok {
 			numHits[search] = hits + strings.Count(newText, search)
@@ -11,8 +14,13 @@ func ExtraStringReplace(text string, extraFindAndReplaces map[string]string, num
 			numHits[search] = strings.Count(newText, search)
 		}
 
-		newText = strings.ReplaceAll(newText, search, replace)
+		stringsToReplace[2*i] = search
+		stringsToReplace[2*i+1] = replace
+		i++
 	}
+
+	var replacer = strings.NewReplacer(stringsToReplace...)
+	newText = replacer.Replace(newText)
 
 	return newText
 }
